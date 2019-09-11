c5b20d3daa4086d40317b70038cd7163322cde52
joepbeijnsberger <joep_beijnsberger@nl.ibm.com>
2019-09-11 16:00:33 +0200

    sixth commit

--- src/controllers/shipping-controller.js
+++ src/controllers/shipping-controller.js
@@ -6,14 +6,17 @@ class ShippingController {
   constructor() {
     this.REGULAR_PRICE = 0.1
     this.OVERNIGHT_PRICE = 1
+	this.OVERPRIZED = 10
   }
 
 async getItemShipping(item) {
   var shippingAmount = await productService.getProductWeight(item.id)
   if (item.type.toLowerCase() === 'overnight') {
     return shippingAmount * this.OVERNIGHT_PRICE
-  } else {
+  } else if (item.type.toLowerCase() === 'regular') {
     return shippingAmount * this.REGULAR_PRICE
+  } else {
+    return shippingAmount * this.OVERPRIZED
   }
 }
 
